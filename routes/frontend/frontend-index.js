09a16ef7ba16c508b825fe165ceab4b1073c5c96
Valentin Wissler <valentinwissler42@outlook.com>
2023-09-14 16:42:23 +0100

    frontend projects up

---
+++ routes/frontend/frontend-index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router({caseSensitive: true})
+
+// using the :name parameter in the url to get the file name
+// this route serves all HTML and CSS files.
+router.get('/:name', (req, res, next) => {
+  console.log('%s %s %s', req.method, req.url, req.path)
+  var options = {
+    root: "public/html/frontend/",
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+  let filename = req.params.name + ".html";
+  res.sendFile(filename , options, function (err) {
+    if(err) {
+        console.log(err);
+        res.status(403).send("Sorry but you shouldn't be here...");
+    }
+    else {
+        console.log('Sent:', filename);
+    }
+  });
+})
+
+module.exports = router
